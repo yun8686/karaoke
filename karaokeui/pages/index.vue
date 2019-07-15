3720d9ac7fbb5a02ffb7bcba3cbb953ea6286f73
yun-pc\yun <yun.mes41@gmail.com>
2019-07-15 22:50:34 +0900

    データ追加

--- karaokeui/pages/index.vue
+++ karaokeui/pages/index.vue
@@ -2,7 +2,7 @@
   <v-container pa-0>
     <v-flex xs12 sm12 md12>
       <v-container>
-        <v-text>カラオケ館 フリータイム </v-text>
+        <h1>カラオケ館 フリータイム </h1>
         <v-text-field key="name" v-model="name" label="店舗名" v-on:change="search"></v-text-field>
         <v-menu
           :close-on-content-click="true"
@@ -33,18 +33,37 @@
           <v-flex xs4 sm4 md4>
            <p>{{this.attributes}}</p>
          </v-flex>
-        </v-layout row wrap>
+
+         <v-flex xs5 sm5 md5 style="align-self: center;">
+           <v-btn-toggle dark v-model="times" multiple>
+             <v-btn  value="daytime" color="orange">
+               昼時間
+             </v-btn>
+             <v-btn value="nighttime" color="blue">
+               夜時間
+             </v-btn>
+           </v-btn-toggle>
+         </v-flex>
+         <v-flex xs7 sm7 md7>
+           <v-switch v-model="freedrink" label="フリードリンクのみ" color="red"></v-switch>
+         </v-flex>
+
+        </v-layout>
       </v-container>
     </v-flex>
-    <v-list two-line>
+    <v-list three-line>
       <template v-for="(dataset, index) in datasets">
         <v-list-tile
-          :key="dataset.id"
+          :key="dataset.id+index"
           @click=""
         >
           <v-list-tile-content>
-            <v-list-tile-title>{{ dataset.name }}</v-list-tile-title>
-            <v-list-tile-sub-title>{{ dataset.address }}</v-list-tile-sub-title>
+            <v-list-tile-title>{{ dataset.name }} ({{dataset.start}}:00 ~ {{dataset.end}}:00)</v-list-tile-title>
+            <v-list-tile-sub-title>会員: 500円　一般: 500円</v-list-tile-sub-title>
+            <v-list-tile-sub-title>学生会員: 500円　学生一般: 500円</v-list-tile-sub-title>
+            <v-list-tile-sub-title style="color:red" v-if="dataset.drink==1">フリードリンク</v-list-tile-sub-title>
+            <v-list-tile-sub-title v-else-if="dataset.drink==0">１ドリンク制</v-list-tile-sub-title>
+            <v-list-tile-sub-title v-else>ドリンク注文不要</v-list-tile-sub-title>
           </v-list-tile-content>
           <v-list-tile-action>
             <v-btn icon>
@@ -66,6 +85,7 @@
 <script>
 import header from '../assets/header.json';
 import tokyo from '../assets/tokyo.json';
+import tokyo_price from '../assets/tokyo_price.json';
 import JapaneseHolidays from 'japanese-holidays';
 
 export default {
@@ -77,9 +97,21 @@ export default {
     member: true,
     attributes: "aaa",
     headers: header,
-    datasets: tokyo,
+    datasets: [],
+    times: ["daytime", "nighttime"],
+    freedrink: false,
   }),
+  mounted: function(){
+    tokyo_price.forEach(w=>{
+      Object.assign(w, tokyo.find(v=>v.id==w.id));
+    });
+    this.search();
+  },
   components: {
+  },
+  watch:{
+    freedrink: function(){this.search()},
+    times: function(){this.search()},
   },
   methods:{
     search(){
@@ -89,17 +121,27 @@ export default {
       const holiday = JapaneseHolidays.isHoliday(date);
       const tomorrowholiday = JapaneseHolidays.isHoliday(tomorrow);
       const jday = JapaneseHolidays.getJDay(date);
+      const freedrink = this.freedrink;
+      const times = this.times; // daytime, nighttime
       this.attributes = `
         祝日: ${holiday}
         祝前日: ${tomorrowholiday}
         曜日: ${jday}
+        フリードリンク: ${freedrink}
+        時間: ${times}
       `;
-      this.datasets = tokyo.filter(v=>{
-        if(nameRegExp.test(v.name)){
-          return true;
-        }else{
-          return false;
+      this.datasets = tokyo_price.filter(v=>{
+        const day = tomorrowholiday?8:(holiday?7:jday);
+        if(!nameRegExp.test(v.name)) return false;
+        if(v.day.indexOf(day)==-1) return false;
+        if(freedrink && v.drink==0) return false;
+        if(times.indexOf('daytime')==-1){
+          if(v.start<16) return false;
         }
+        if(times.indexOf('nighttime')==-1){
+          if(v.start>=16) return false;
+        }
+        return true;
       });
     }
   }
